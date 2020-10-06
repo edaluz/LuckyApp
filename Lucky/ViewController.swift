81d39c49f9b7f5ed35fc84044831794a74128b6b
edaluzrappi <emiliano.daluz@rappi.com>
2020-10-06 20:49:10 -0300

    Initial Commit

---
+++ Lucky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lucky
+//
+//  Created by Emiliano Da luz on 06/10/2020.
+//  Copyright © 2020 MEK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
